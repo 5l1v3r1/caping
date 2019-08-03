545323b7644329d0cc1cd3a42f8f896635196908
ColdDark29 <53429191+ColdDark29@users.noreply.github.com>
2019-08-03 15:38:32 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?PHP
+/*
+Akun tunggal
+"u=xxxxxxxx;n=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+Akun ganda
+"u=xxxxxxxx;n=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+"u=xxxxxxxx;n=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+*/
+
+$config =
+[
+"u=xxxxxxxx;n=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+]
+?>

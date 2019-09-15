d3c80bd400dd13e461afa7e649321acd801b1f94
ColdDark29 <53429191+ColdDark29@users.noreply.github.com>
2019-09-15 15:21:34 +0800

    Update config.php

--- config.php
+++ config.php
@@ -6,8 +6,9 @@ Satu akun
 Multi akun
 "u=xxxxxxxx;n=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
 "u=xxxxxxxx;n=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-*/
 
+#Satu user agent untuk semua akun
+$user_agent = "";
 $config =
 [
 "u=xxxxxxxx;n=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
